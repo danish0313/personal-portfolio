c98f7acf0df88259d5c7322c83dc390efb84adf6
danish0313 <danishoo.dk@gmail.com>
2019-08-03 23:42:22 +0500

    Updates

---
+++ precache-manifest.9ff4a561883f44318cf103eb8d43049b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a05fa6a8d7eec4f12fd65c97cf4abc",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "4b981bebb431b278eb30",
+    "url": "/personal-portfolio/static/css/main.62707a32.chunk.css"
+  },
+  {
+    "revision": "b1197dbe7c26956f7010",
+    "url": "/personal-portfolio/static/js/2.3ba4f1ce.chunk.js"
+  },
+  {
+    "revision": "4b981bebb431b278eb30",
+    "url": "/personal-portfolio/static/js/main.dcae5566.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "db57b69ee2bc54a6910a601628908762",
+    "url": "/personal-portfolio/static/media/java.db57b69e.png"
+  },
+  {
+    "revision": "e2869a4f6f244073779fb7b98c1d395b",
+    "url": "/personal-portfolio/static/media/logo.e2869a4f.png"
+  },
+  {
+    "revision": "72808ec0f39241421557b0da7aba62a2",
+    "url": "/personal-portfolio/static/media/node.72808ec0.png"
+  },
+  {
+    "revision": "0e1513a50468044ab95624fa1746c43d",
+    "url": "/personal-portfolio/static/media/overgrown.0e1513a5.jpg"
+  },
+  {
+    "revision": "1e71b45938cc2ffd396886c5ffae279f",
+    "url": "/personal-portfolio/static/media/react.1e71b459.png"
+  }
+]);
